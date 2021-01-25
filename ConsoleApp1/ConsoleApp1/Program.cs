cae2005fcefcaf9c7b6ea5fcde83ee182c36598b
Berkay <berkaydyk1905@gmail.com>
2021-01-26 00:48:00 +0300

    Kürenin Hacmini Bulan Program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416;
+            int r = 50;
+            double V = 4 / 3 * pi * (r * r * r);
+
+            Console.WriteLine("Kürenin Hacmi = "+V);
+            Console.ReadKey();
+
+
+        }
+    }
+}
